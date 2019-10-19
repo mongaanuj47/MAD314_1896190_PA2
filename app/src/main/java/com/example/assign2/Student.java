b130fe0e650d18cf3f9ecf50fc55189b99e20f31
mongaanuj47 <mongaanuj47@yahoo.com>
2019-10-19 12:30:46 -0400

    final

---
+++ app/src/main/java/com/example/assign2/Student.java
@@ -0,0 +1,7 @@
+package com.example.assign2;
+// POJO
+public class Student {
+        String name;
+        int id;
+        String password;
+}
---
+++ app/src/main/java/com/example/assign2/StudentDataSource.java
@@ -0,0 +1,3 @@
+package com.example.assign2;
+//Singleton
+
